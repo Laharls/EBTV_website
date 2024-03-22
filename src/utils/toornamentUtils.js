6648043ad1833e24e9ec5f274bfd2c2205da9081
Anthony <anthony089@outlook.fr>
2024-03-22 19:29:39 +0100

    Remove deleted files

--- src/utils/toornamentUtils.js
+++
@@ -1,8 +0,0 @@
-async function getRank(tournamentId, stageIds) {
-    const rank = await fetch(`http://localhost:8000/api/toornament/v2/rank?tournament_ids=${tournamentId}&stage_ids=${stageIds}`);
-    return rank.json();
-  }
-
-  module.exports = {
-    getRank,
-  }
