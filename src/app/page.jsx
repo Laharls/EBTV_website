a7489e4195dfdf30be225407b3a0b9c46bf80c8a
Vapoor <kylian.gagnant@orange.fr>
2024-10-30 11:21:58 +0100

    Added website maintenance text
    Small page.jsx change to show website maintenance

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import PreviewRank from "@/components/ranks/previewRank";
 import { toornamentIdLeagueS2, divisionIdLeagueS2 } from '@/data/toornamentId';
+import { color } from "framer-motion";
 
 const Homepage = async () => {
   return (
@@ -17,8 +18,9 @@ const Homepage = async () => {
             unoptimized />
 
           <div className="flex flex-col justify-center items-center gap-4">
-            <p className="font-bold text-xl dark:text-white">Saison 2</p>
-            <p className="font-bold text-xl text-center dark:text-white">11 divisions, 88 équipes, 520 joueurs !</p>
+            <p className="font-bold text-xl dark:text-white">Saison 3</p>
+            <p className="font-bold text-xl text-center dark:text-white">12 divisions, 75 équipes, 440 joueurs !</p>
+            <p className="font-bold text-xl text-center dark:text-white"> Un peu de patience ! Le site sera de retour bientôt ! Vous pouvez retrouver les infos <a href="https://play.toornament.com/fr/tournaments/8037985823043919872/" style={{color:'red'}}>ici</a></p>;
           </div>
         </div>
 
